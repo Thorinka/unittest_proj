6ad8cfa1c0bb17f1a4d653c35aa35782a6391f31
Thorinka <mmlentyshenko@gmail.com>
2023-04-02 17:30:47 +0300

    fixed test coverage, 100% covered

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,7 +7,11 @@ class TestArrs(unittest.TestCase):
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -4), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -3), [1, 2, 3])
